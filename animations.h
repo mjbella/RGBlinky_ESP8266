beacb2d157782269a34a8e66e6d1f5d2908228c0
Michael Bella <mjbella@sbcglobal.net>
2017-06-25 16:48:09 -0700

    initial thoughts on animation framework

--- animations.h
+++ animations.h
@@ -1,1 +1,25 @@
 # Definitions for the animation engine go here!
+
+/* Animation Sequence Format (Struct)
+ * Effect Type
+ * Delay
+ * nLoops
+ * nLEDS
+ * Repeat Pattern?
+ * RGB LED 0
+ * RGB LED 1
+ * ...
+ * RGB LED n
+ * 
+ */
+
+/* Animation Types
+ * None - Just update leds based on led data.
+ * Blank - Set all off
+ * 
+ *
+ */
+
+typedef enum {} animation_effect;
+
+
